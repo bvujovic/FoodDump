98fd72b44debdfb66373fee007a3ad8b9038f0a3
bvujovic <bv.net@outlook.com>
2021-09-12 06:47:23 +0200

    Added time/delayed long pull. Added MQTT/WiFi status LED. Smaller code improvements. AIO_dashboard.png updated.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,22 @@
 
 WiFiClient wiFiClient;
 PubSubClient mqtt(wiFiClient);
+typedef unsigned int uint;
 void callback(char *topic, byte *data, uint length);
 
 #define FEED_SHORT_DUMP "food-dump-short"
 #define FEED_LONG_DUMP "food-dump-long"
-const int pinSolenoid = D1;
-const int pinModeSwitch = D2; // Prekidac za izbor moda: MQTT ili touch.
-const int SHORT_PULL = 75;        // Kratko aktiviranje solenoida (ispustanje hrane).
-const int LONG_PULL = 300;        // Dugo aktiviranje solenoida (ispustanje hrane).
-const int MAX_SOLENOID_ON = 2000; // Maksimalno trajanje (u ms) aktiviranog/povucenog solenoida.
-uint itvPrepareToDump;            // Trajanje (u ms) cekanja pred sipanje hrane.
+#define FEED_LONG_DELAYED "food-dump-time"
+const uint SEC = 1000;
+const uint MIN = 60 * SEC;
+const byte pinSolenoid = D1;
+const byte pinModeSwitch = D2;       // Prekidac za izbor moda: MQTT ili touch.
+const byte pinWiFi = D8;             // LED indikator WiFi-a (MQTT_Mode);
+const int SHORT_PULL = 75;           // Kratko aktiviranje solenoida (ispustanje hrane).
+const int LONG_PULL = 300;           // Dugo aktiviranje solenoida (ispustanje hrane).
+const int MAX_SOLENOID_ON = SEC; // Maksimalno trajanje (u ms) aktiviranog/povucenog solenoida.
+uint itvPrepareToDump;               // Trajanje (u ms) cekanja pred sipanje hrane.
+uint msFoodDrop = 0;                 // Vreme za prosipanje hrane kada se koristi time/delayed MQTT opcija.
 
 // Moguce vrednosti za dugmice u AIO FoodDump dashboard-u.
 enum ButtonStatus
@@ -32,12 +38,28 @@ enum DeviceMode
 } deviceMode;
 
 #include "Blinky.h"
-Blinky led(LED_BUILTIN, false, 1000, 3);
+Blinky led(LED_BUILTIN, false, SEC, 3);
+Blinky ledWiFi(pinWiFi, true);
 
 void solenoidON(bool on)
 {
     digitalWrite(pinSolenoid, on);
     led.ledOn(on);
+}
+
+// Priprema (paljenje LEDa) i pull solenoida (trajanje zadato sa ms).
+void solenoidPull(uint ms)
+{
+    // cekanje da se korisnik pripremi za gledanje prosipanja hrane
+    led.on();
+    delay(itvPrepareToDump);
+    led.off();
+    delay(SEC);
+    Serial.println("Solenoid pull");
+    // prosipanje hrane omalenom kunikulusu
+    solenoidON(true);
+    delay(ms);
+    solenoidON(false);
 }
 
 void reconnect()
@@ -48,7 +70,6 @@ void reconnect()
         ESP.reset();
     }
 
-    // Loop until we're reconnected
     while (!mqtt.connected())
     {
         Serial.println("MQTT connecting...");
@@ -57,7 +78,8 @@ void reconnect()
             Serial.println("Connected");
             mqtt.subscribe(AIO_USER AIO_FEEDS FEED_SHORT_DUMP, 0);
             mqtt.subscribe(AIO_USER AIO_FEEDS FEED_LONG_DUMP, 0);
-            led.ledOn(false);
+            mqtt.subscribe(AIO_USER AIO_FEEDS FEED_LONG_DELAYED, 0);
+            led.off();
         }
         else
         {
@@ -70,8 +92,6 @@ void reconnect()
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    led.ledOn(false);
     pinMode(pinSolenoid, OUTPUT);
     pinMode(pinModeSwitch, INPUT_PULLUP);
     delay(100);
@@ -79,7 +99,7 @@ void setup()
 
     if (deviceMode == TouchMode)
     {
-        itvPrepareToDump = 1000; // cekanje da bi se izbegao burst ispaljivanja granula zbog nestabilnog signala
+        itvPrepareToDump = SEC; // cekanje da bi se izbegao burst ispaljivanja granula zbog nestabilnog signala
         delay(itvPrepareToDump);
         solenoidON(true);
         delay(SHORT_PULL);
@@ -89,8 +109,9 @@ void setup()
     if (deviceMode == MQTT_Mode)
     {
         Serial.begin(115200);
-        itvPrepareToDump = 5000; // cekanja da se korisnik spremi za posmatranje sipanja hrane
-        led.ledOn(true);
+        itvPrepareToDump = 5 * SEC; // cekanja da se korisnik spremi za posmatranje sipanja hrane
+        led.on();
+        ledWiFi.on();
         WiFi.mode(WIFI_STA);
         ConnectToWiFi();
         Serial.print("\nWiFi connected, IP address: ");
@@ -107,29 +128,44 @@ void loop()
         if (!mqtt.connected())
             reconnect();
         mqtt.loop();
+
+        if (msFoodDrop != 0 && millis() >= msFoodDrop)
+        {
+            solenoidPull(LONG_PULL);
+            msFoodDrop = 0;
+        }
     }
     delay(100);
 }
 
+uint dataToInt(byte *data, uint length)
+{
+    uint x = 0;
+    for (size_t i = 0; i < length; i++)
+        x = x * 10 + data[i] - '0';
+    return x;
+}
+
 void callback(char *topic, byte *data, uint length)
 {
-    //T Serial.println(topic);
-    //T Serial.println((int)data[0]);
-    int ms = -1; // trajanje (u ms) povlacenja poluge solenoida
+    Serial.println(topic);
+    for (size_t i = 0; i < length; i++)
+        Serial.printf("%c", data[i]);
+    Serial.println();
+
+    msFoodDrop = 0;
+    bool isDelayedPull = strstr(topic, "time");
+    if (isDelayedPull)
+    {
+        uint itv = dataToInt(data, length) * MIN;
+        msFoodDrop = millis() + itv;
+    }
+
+    uint ms = 0; // trajanje (u ms) povlacenja poluge solenoida
     if (data[0] == BTN_SHORT)
         ms = SHORT_PULL;
     if (data[0] == BTN_LONG)
         ms = LONG_PULL;
-    if (ms > 0 && ms <= MAX_SOLENOID_ON)
-    {
-        // cekanje da se korisnik pripremi za gledanje prosipanja hrane
-        led.ledOn(true);
-        delay(itvPrepareToDump);
-        led.ledOn(false);
-        delay(1000);
-        // prosipanje hrane omalenom kunikulusu
-        solenoidON(true);
-        delay(ms);
-        solenoidON(false);
-    }
+    if (ms > 0 && ms <= MAX_SOLENOID_ON && !isDelayedPull)
+        solenoidPull(ms);
 }
